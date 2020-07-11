24a1b64a312953ff93604c7df5d894f92821e40a
Lalitus <tallitosan@gmail.com>
2020-07-11 04:24:46 -0500

    added result template functins, calculate values & minor tweaks

--- app/public/js/script.js
+++ app/public/js/script.js
@@ -1,13 +1,15 @@
 const ham = document.getElementById('ham');
 const menu = document.getElementById('navbarBasicExample');
-const section = document.getElementsByTagName('section');
-const footer = document.querySelectorAll('.footer')
-const date = document.getElementById('date');
 const modalClose = document.getElementsByClassName('modal-close');
 const btnClose = document.getElementsByClassName('closeBtn');
 const deleteIngredientBtn = document.getElementsByClassName('deleteIngredientBtn');
-const editIngredientBtn = document.getElementsByClassName('editIngredient');
-const editIngredientValue = document.getElementsByClassName('editValue');
+const quantity = document.getElementsByClassName('quantity');
+const ingredient = document.getElementsByClassName('ingredient');
+const calculate = document.getElementById('calculate');
+const wishValue = document.getElementById('wishValue');
+const result = document.getElementById('result');
+const again = document.getElementById('again');
+const modal = document.querySelector('.modal');
 
 // open/close menu
 ham.addEventListener('click', () => {
@@ -18,18 +20,19 @@ ham.addEventListener('click', () => {
 // open/close modal
 for (let i = 0; i < modalClose.length; i++) {
     modalClose[i].addEventListener('click', () => {
-        document.querySelector('.modal').classList.remove('is-active');
+        modal.classList.remove('is-active');
     });
 
     btnClose[i].addEventListener('click', () => {
-        document.querySelector('.modal').classList.remove('is-active');
+        modal.classList.remove('is-active');
     });
 
     deleteIngredientBtn[i].addEventListener('click', () => {
-        document.querySelector('.modal').classList.add('is-active');
+        modal.classList.add('is-active');
     });
 }
 
+// to edit ingredient unit
 function submitEditIngredient(editIngredient){
     const pureId = editIngredient.slice(17)
     const value = document.getElementById(`editValue-${pureId}`)
@@ -37,15 +40,65 @@ function submitEditIngredient(editIngredient){
     const form = document.getElementById(`editForm-${pureId}`)
 
     input.value = value.value;
-    // console.log(input)
-    // console.log(value)
-    // console.log(form)
     form.submit();
-
-    
 }
 
+// reload page
+again.addEventListener('click', () => {
+    location.reload();
+    console.log(again)
+})
+
+// calculate values
+calculate.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    let rawValues = [];
+    
+    for (let i = 0; i < quantity.length; i++) {
+        rawValues.push(quantity[i].value * wishValue.value)
+
+        const htmlString = resultTemplate(rawValues[i], ingredient[i].innerText)
+        const resultElement = createTemplate(htmlString)
+        calculate.setAttribute('disabled', true)
+
+        result.append(resultElement);
+    }
+});
+    
+// create html template
+function createTemplate(HTMLString){
+    const html = document.implementation.createHTMLDocument();
+    html.body.innerHTML = HTMLString;
+    return html.body.children[0]
+}
+
+// result template
+function resultTemplate(quantity, ingredient){
+    return `
+    <div class="card">
+        <header class="card-header">
+            <p class="card-header-title is-relative" style="width: 100%;">${ingredient}</p>
+    
+            <div class="field is-horizontal">
+                <div class="field-label is-normal"><label class="label">Unidades</label></div>
+    
+                <div class="field-body">
+                    <div class="field">
+                        <p class="control is-expanded has-icons-left">
+                            <input class="input " type="text" placeholder="Name" value="${quantity}">
+                        </p>
+                    </div>
+                </div>
+    
+            </div>
+            <div class="card-footer-item"></div>
+            <div class="card-footer-item editIngredient"><span class="icon has-text-success"><i class="fas fa-arrow-left"></i></span></div>
+        </header>
+    </div>
+    `
+}
 
 // footer year
-date.innerText = new Date().getFullYear();
+document.querySelectorAll('.footer').innerText = new Date().getFullYear();
 
